0455455ccaf56ff4eef81054963aa1b0cdfb2359
Gregg Hogan <21093023+greggoryhogan@users.noreply.github.com>
2022-05-11 16:16:42 -0400

    Add viewport to meta

--- wp-content/themes/zone-creative/header.php
+++ wp-content/themes/zone-creative/header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
 	<?php wp_head(); ?>
 </head>
 
