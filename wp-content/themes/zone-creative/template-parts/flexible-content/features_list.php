de7e72dafea9df585fb8dc7b4f73044dd9a52105
Gregg Hogan <21093023+greggoryhogan@users.noreply.github.com>
2022-06-06 14:13:10 -0400

    Css tweaks to theme

--- wp-content/themes/zone-creative/template-parts/flexible-content/features_list.php
+++ wp-content/themes/zone-creative/template-parts/flexible-content/features_list.php
@@ -14,12 +14,25 @@ if( have_rows('features') ):
             $heading = get_sub_field( 'heading' );
             $content = get_sub_field('content');    
             $link = get_sub_field('link');
+            if( $link ): 
+                $link_url = $link['url'];
+                $link_title = $link['title'];
+                $link_target = $link['target'] ? $link['target'] : '_self';
+            endif;
             if($heading != '') {
                 echo '<div class="feature-heading"';
                 if($column_1_animation != 'none') {
                     echo ' data-aos="'.$column_1_animation.'" data-aos-easing="'.$column_animation_easing.'" data-aos-anchor-placement="'.$column_animation_anchor_placement.'" data-aos-duration="'.$column_animation_easinganimation_speed.'"';
                 }
-                echo '><h3>'.zone_content_filters($heading).'</h3></div>';
+                echo '>';
+                if( $link ): 
+                    echo '<a class="block-link" href="'.esc_url( $link_url ).'" target="'.esc_attr( $link_target ).'">';
+                endif;
+                    echo '<h3>'.zone_content_filters($heading).'</h3>';
+                if( $link ): 
+                    echo '</a>';
+                endif;
+                echo '</div>';
             }
             if($content != '' || $link != '') {
                 echo '<div class="feature-content';
@@ -33,9 +46,6 @@ if( have_rows('features') ):
                     echo zone_content_filters($content);
                 }
                 if( $link ): 
-                    $link_url = $link['url'];
-                    $link_title = $link['title'];
-                    $link_target = $link['target'] ? $link['target'] : '_self';
                     echo '<div class="link">';
                         echo '<a class="btn" href="'.esc_url( $link_url ).'" target="'.esc_attr( $link_target ).'">'.zone_content_filters(esc_html( $link_title )).'</a>';
                     echo '</div>';
